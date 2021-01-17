5cd0d1a45003fa1e0b64b0808198f8279b252eb2
Wiktor Szlegier <22wiktors@gmail.com>
2021-01-17 16:57:56 +0100

    Create WeeklyDetails view

---
+++ WeatherApp/DailyWeatherCell.swift
@@ -0,0 +1,16 @@
+//
+//  DailyWeatherCell.swift
+//  WeatherApp
+//
+//  Created by Wiktor Szlegier on 17/01/2021.
+//
+
+import UIKit
+
+class DailyWeatherCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dayOfAWeek: UILabel!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var temperature: UILabel!
+    
+}
--- WeatherApp/WeeklyDetailsViewController.swift
+++ WeatherApp/WeeklyDetailsViewController.swift
@@ -7,23 +7,71 @@
 
 import UIKit
 
-class WeeklyDetailsViewController: UIViewController {
+class WeeklyDetailsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var days = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
+        defineDaysArray()
+        
+        // TODO: Fix styling issues
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func defineDaysArray() {
+        let day = Calendar.current.component(.weekday, from: Date())
+        
+        for i in 0...6 {
+            var numberOfTheDay = day + i + 1
+            if numberOfTheDay > 7 {
+                numberOfTheDay -= 7
+            }
+            days.append(getNameOfTheDay(numberOfTheDay))
+        }
     }
-    */
+    
+    func getNameOfTheDay(_ day: Int) -> String {
+        switch day {
+        case 1:
+            return "Niedziela"
+        case 2:
+            return "Poniedziałek"
+        case 3:
+            return "Wtorek"
+        case 4:
+            return "Środa"
+        case 5:
+            return "Czwartek"
+        case 6:
+            return "Piątek"
+        case 7:
+            return "Sobota"
+        default:
+            return "Error"
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DailyWeather", for: indexPath) as! DailyWeatherCell
+        
+        cell.dayOfAWeek.text = days[indexPath.item]
+        cell.temperature.text = "-- °C"
+        cell.weatherIcon.image = UIImage(named: "clouds")
+        
+        // TODO: Check width of the cell
+        
+        //cell.frame.size.width = UIScreen.main.bounds.width * 0.6
+        
+        return cell
+    }
 
 }
